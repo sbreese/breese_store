604d1e8d226b6801aa209e3b1e78ccfa417ab42a
steve_breese <steve.breese@hs2solutions.com>
2019-03-23 14:01:50 -0500

    make it better

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(result => {
     app.listen(process.env.PORT || 3000);
   })
