aecb7c84108988b19b64f3e31dffa947612c624f
steve_breese <steve.breese@hs2solutions.com>
2019-03-25 13:45:58 -0500

    make it better

--- controllers/auth.js
+++ controllers/auth.js
@@ -177,29 +177,30 @@ exports.postLogin = (req, res, next) => {
         .compare(password, user.password)
         .then(doMatch => {
           if (doMatch) {
+            return user;
+          }
+        })
+        .then(user => {
             req.session.isLoggedIn = true;
             req.session.user = user;
+            // steves additions
+            Order.find({ 'user.userId': user._id })
+            .then(orders => {
+              console.log("Here is the raw orders for " + user._id);
+              console.log(orders);
+              req.session.user.raw_orders = orders;
 
-                        // steves additions
-                        Order.find({ 'user.userId': user._id })
-                        .then(orders => {
-                          console.log("Here is the raw orders for " + user._id);
-                          console.log(orders);
-                          req.session.user.raw_orders = orders;
-            
-                          return req.session.save(err => {
-                            console.log(err);
-                            res.redirect('/');
-                          });
+              return req.session.save(err => {
+                console.log(err);
+                res.redirect('/');
+              });
 
-                        })
-                        .catch(err => {
-                          console.log(err);
-                          res.redirect('/login');
-                        });
-                        // end steves addition
-
-
+            })
+            .catch(err => {
+              console.log(err);
+              res.redirect('/login');
+            });
+            // end steves addition
           }
           return res.status(422).render('auth/login', {
             path: '/login',
