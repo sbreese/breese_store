6d0ada4c412bf0f70fafb997857db750d5f25ec6
steve_breese <steve.breese@hs2solutions.com>
2019-03-27 17:52:00 -0500

    make it better

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const usersController = require('../controllers/users');
 const isAuth = require('../middleware/is-auth');
 
-// /admin/users => GET
-router.get('/users', isAuth, usersController.getUsers);
 // /admin/users/:userId => GET
 router.get('/users/:userId', isAuth, usersController.getUsers);
+// /admin/users => GET
+router.get('/users', isAuth, usersController.getUsers);
 // /admin/user => DELETE
 router.delete('/user/:userId', isAuth, usersController.deleteUser);
 
