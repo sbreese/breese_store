9e8c5925c09d83fcd211c18981727620badf8087
steve_breese <steve.breese@hs2solutions.com>
2019-03-27 11:25:52 -0500

    make it better

---
+++ controllers/orders.js
@@ -0,0 +1,38 @@
+const User = require('../models/user');
+
+const ITEMS_PER_PAGE = 10;
+
+exports.getAllOrders = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  User.find()
+    .countDocuments()
+    .then(numUsers => {
+      totalItems = numUsers;
+      return User.find()
+        .populate('orders')
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
+    .then(users => {
+      res.render('auth/user-list', {
+        users,
+        pageTitle: 'Users',
+        path: '/users',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      console.log("OOPS, an error:");
+      console.log(error);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
