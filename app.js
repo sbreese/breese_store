9bb0c29adb5939dda8dca4790db19c6f74380eca
steve_breese <steve.breese@hs2solutions.com>
2019-03-27 15:49:06 -0500

    make it better

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use('/admin', orderRoutes);
-app.use('/users', usersRoutes);
+app.use('/admin', usersRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const usersController = require('../controllers/users');
 const isAuth = require('../middleware/is-auth');
 
+// /admin/users => GET
 router.get('/users', isAuth, usersController.getUsers);
+// /admin/user => DELETE
 router.delete('/user/:userId', isAuth, usersController.deleteUser);
 
 module.exports = router;
