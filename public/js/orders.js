fcc0856103f6d9b393a4a47cd01398f730976b17
steve_breese <steve.breese@hs2solutions.com>
2019-03-28 12:07:37 -0500

    make it better

--- public/js/orders.js
+++ public/js/orders.js
@@ -1,3 +1,10 @@
+function hasClass(el, className)
+{
+    if (el.classList)
+        return el.classList.contains(className);
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+}
+
 function addClass(el, className)
 {
     if (el.classList)
