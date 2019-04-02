55877d014b90cfabff5ace7132bcb789968ad6d9
steve_breese <steve.breese@hs2solutions.com>
2019-04-01 23:10:53 -0500

    make it better

--- util/file.js
+++ util/file.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const deleteFile = (filePath) => {
     fs.unlink(filePath, (err) => {
         if (err) {
-            throw (err);
+            // throw (err); <-- Throws error when file is already gone.  We can ignore instead.
+            console.log("Error deleting file: ", err);
         }
     });
 }
