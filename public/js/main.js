690dd444de5a1475517e1b4728ed195d8ea4dff2
steve_breese <steve.breese@hs2solutions.com>
2019-04-02 11:42:51 -0500

    make it better

--- public/js/main.js
+++ public/js/main.js
@@ -272,6 +272,7 @@
         e.preventDefault();
         console.log("Hi Steve, here is the this for what you just clicked!");
         console.log($(this));
+        console.log($(this).id);
         $('.js-modal1').addClass('show-modal1');
     });
 
