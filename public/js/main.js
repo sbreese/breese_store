3af1df5790385c794c8dce3e55b4c57658dbfdbd
steve_breese <steve.breese@hs2solutions.com>
2019-04-03 17:49:24 -0500

    make it better

--- public/js/main.js
+++ public/js/main.js
@@ -211,14 +211,40 @@
 
     /*==================================================================
     [ +/- num product ]*/
+    const changeQuantity = (btn, qtyChange) => {
+        const prodId = btn.parentNode.querySelector('[name=productId]').value;
+        const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+      
+        const productElement = btn.closest('article');
+      
+        fetch('/admin/product/' + prodId, {
+          method: 'DELETE',
+          headers: {
+            'csrf-token': csrf
+          }
+        })
+          .then(result => {
+            return result.json();
+          })
+          .then(data => {
+            console.log(data);
+            productElement.parentNode.removeChild(productElement);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      };
+
     $('.btn-num-product-down').on('click', function(){
         var numProduct = Number($(this).next().val());
         if(numProduct > 0) $(this).next().val(numProduct - 1);
+        changeQuantity($(this), -1);
     });
 
     $('.btn-num-product-up').on('click', function(){
         var numProduct = Number($(this).prev().val());
         $(this).prev().val(numProduct + 1);
+        changeQuantity($(this), 1);
     });
 
     /*==================================================================
