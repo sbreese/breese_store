df2117766e68a67643d63162a7edc189acfaa8e5
steve_breese <steve.breese@hs2solutions.com>
2019-04-02 22:24:00 -0500

    make it better

--- controllers/auth.js
+++ controllers/auth.js
@@ -290,9 +290,9 @@ exports.postLogin = (req, res, next) => {
             req.session.user = user;
             req.session.login_orders = orders;
 console.log("OK, lets save this shit!");
-console.log(req.session.user.cart);
+console.log(req.session.user.cart[0].product);
 console.log("Should equal:");
-console.log(user.cart);
+console.log(user.cart[0].product);
             return req.session.save(err => {
               console.log(err);
               res.redirect(user.cart.items.length > 0 ? '/cart' : '/');
