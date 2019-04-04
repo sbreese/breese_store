104a6d0ce72d47c1164ebdba2a7704171f51a242
steve_breese <steve.breese@hs2solutions.com>
2019-04-04 17:16:24 -0500

    make it better

--- public/js/main.js
+++ public/js/main.js
@@ -212,6 +212,7 @@
     /*==================================================================
     [ +/- num product ]*/
     const changeQuantity = (btn, qtyChange) => {
+        console.log(btn);
         const prodId = btn.parentNode.querySelector('[name=productId]').value;
         const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
       
