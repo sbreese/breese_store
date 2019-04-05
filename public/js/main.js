15c4a8adf022a0d7956ff9a0ef412b8426981a07
steve_breese <steve.breese@hs2solutions.com>
2019-04-05 14:02:23 -0500

    make it better

--- public/js/main.js
+++ public/js/main.js
@@ -263,7 +263,7 @@
         var numProduct = Number($(this).prev().val());
         $(this).prev().val(++numProduct);
         // alert($(this).parent().prop("tagName")); // parent().parent().parent().find('[name=num-product]').val(numProduct);
-        alert($(this).parent().attr('class').split(/\s+/)):
+        alert($(this).parent().attr('class').split(/\s+/));
     });
 
     /*==================================================================
