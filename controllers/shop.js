9a16743858ac7cd30684944f2f8d0d5d0fb809f2
steve_breese <steve.breese@hs2solutions.com>
2019-04-08 23:28:47 -0500

    make it better

--- controllers/shop.js
+++ controllers/shop.js
@@ -323,7 +323,7 @@ exports.getProductDetail = (req, res, next) => {
 exports.getBlog = (req, res, next) => {
 
   console.log("We should at least see this!");
-  getShoppingCartData(req)
+  this.getShoppingCartData(req)
     .then(user_cart => {
       console.log("What is user cart?");
       console.log(user_cart);
