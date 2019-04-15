ec72b518c5efc6a8834808228b21ec936350842e
steve_breese <steve.breese@hs2solutions.com>
2019-04-15 16:44:03 -0500

    make it better

--- controllers/helper.js
+++ controllers/helper.js
@@ -16,6 +16,6 @@ exports.calcTotalPrice = cart_items => {
     return this.formatter.format(total);
 }
 
-exports.formatResultInfo = (param_1_value, ITEMS_PER_PAGE, totalItems, page) => {
-    return param_1_value && `<span>${(ITEMS_PER_PAGE < totalItems ? `${(page - 1) * ITEMS_PER_PAGE + 1}-${ITEMS_PER_PAGE} of ` : '' ) + `${totalItems} result` + (totalItems > 1 ? 's' : '') + (param_1_value ? ` for </span><span> </span><span class="a-color-state a-text-bold"><q>${param_1_value}</q></span>` : '</span>')}`;
+exports.formatResultInfo = (param_1_key, param_1_value, ITEMS_PER_PAGE, totalItems, page) => {
+    return param_1_value && `<span>${(ITEMS_PER_PAGE < totalItems ? `${(page - 1) * ITEMS_PER_PAGE + 1}-${ITEMS_PER_PAGE} of ` : '' ) + `${totalItems} result` + (totalItems > 1 ? 's' : '') + (param_1_value ? ' ' + (param_1_key === 'sort_by' ? 'sorted by' : 'for') + ` </span><span> </span><span class="a-color-state a-text-bold"><q>${param_1_value}</q></span>` : '</span>')}`;
 }
